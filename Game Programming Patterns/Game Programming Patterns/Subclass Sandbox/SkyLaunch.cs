462c0d461db664307333fef8dc41ed891e550500
Jacob Malter <jdmalter@ncsu.edu>
2016-09-07 01:05:07 -0400

    Added subclass sandbox.

---
+++ Game Programming Patterns/Game Programming Patterns/Subclass Sandbox/SkyLaunch.cs
@@ -0,0 +1,29 @@
+﻿namespace Game_Programming_Patterns.Subclass_Sandbox
+{
+    public class SkyLaunch : SuperPower
+    {
+        protected override void Activate()
+        {
+            if (Z == 0)
+            {
+                // On the ground, so spring into the air.
+                PlaySound(0, 1.0f);
+                SpawnParticles(1, 10);
+                Z = 20;
+            }
+            else if (Z < 10.0f)
+            {
+                // Near the ground, so doa double jump.
+                PlaySound(1, 1.0f);
+                Z -= 20;
+            }
+            else
+            {
+                // Way up in the air, so do a dive attack.
+                PlaySound(2, 0.7f);
+                SpawnParticles(0, 1);
+                Z = 0;
+            }
+        }
+    }
+}
---
+++ Game Programming Patterns/Game Programming Patterns/Subclass Sandbox/SuperPower.cs
@@ -0,0 +1,23 @@
+﻿namespace Game_Programming_Patterns.Subclass_Sandbox
+{
+    public abstract class SuperPower
+    {
+        protected abstract void Activate();
+
+        protected double X { get; set; }
+
+        protected double Y { get; set; }
+
+        protected double Z { get; set; }
+
+        protected void PlaySound(int sound, double volume)
+        {
+
+        }
+
+        protected void SpawnParticles(int type, int count)
+        {
+
+        }
+    }
+}
