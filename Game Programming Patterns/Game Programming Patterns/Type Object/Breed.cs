5d43ae8d785d485ff9cb203a04a65251ad0b2df0
Jacob Malter <jdmalter@ncsu.edu>
2016-09-07 11:57:50 -0400

    Added type object example.

---
+++ Game Programming Patterns/Game Programming Patterns/Type Object/Breed.cs
@@ -0,0 +1,25 @@
+﻿namespace Game_Programming_Patterns.Type_Object
+{
+    public class Breed
+    {
+        public Breed(Breed parent, int health, string attack)
+        {
+            if (parent != null)
+            {
+                Health = health == 0 ? parent.Health : health;
+                Attack = attack == null ? parent.Attack : attack;
+            }
+        }
+
+        // Starting health
+        public int Health { get; private set; }
+
+        // Message displayed when attacking
+        public string Attack { get; private set; }
+
+        public Monster newMonster()
+        {
+            return new Monster(this);
+        }
+    }
+}
---
+++ Game Programming Patterns/Game Programming Patterns/Type Object/Monster.cs
@@ -0,0 +1,19 @@
+﻿namespace Game_Programming_Patterns.Type_Object
+{
+    public class Monster
+    {
+        private Breed _breed;
+
+        public Monster(Breed breed)
+        {
+            _breed = breed;
+            Health = breed.Health;
+        }
+
+        // current health
+        public int Health { get; set; }
+
+        // use breed's attack string
+        public string Attack { get { return _breed.Attack; } }
+    }
+}
