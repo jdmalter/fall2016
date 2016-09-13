3e6c5a881b03eafcb4c2ea7b597b66de7a8d5d75
Jacob Malter <jdmalter@ncsu.edu>
2016-09-13 00:49:26 -0400

    Added dirty flag.

---
+++ Game Programming Patterns/Game Programming Patterns/Dirty Flag/GraphNode.cs
@@ -0,0 +1,34 @@
+﻿namespace Game_Programming_Patterns.Dirty_Flag
+{
+    public class GraphNode
+    {
+        private static readonly int MAX_CHILDREN;
+
+        private Transform _world;
+        private bool _dirty = true;
+        private Transform _local = Transform.Origin();
+        private GraphNode[] _children = new GraphNode[MAX_CHILDREN];
+        private int _numChildren;
+
+        public void SetTransform(Transform local)
+        {
+            _local = local;
+            _dirty = true;
+        }
+
+        public void Render(Transform parentWorld, bool dirty)
+        {
+            dirty |= _dirty;
+            if (dirty)
+            {
+                _world = _local.Combine(parentWorld);
+                dirty = false;
+            }
+
+            foreach (GraphNode child in _children)
+            {
+                child.Render(_world, dirty);
+            }
+        }
+    }
+}
---
+++ Game Programming Patterns/Game Programming Patterns/Dirty Flag/Transform.cs
@@ -0,0 +1,15 @@
+﻿namespace Game_Programming_Patterns.Dirty_Flag
+{
+    public class Transform
+    {
+        public static Transform Origin()
+        {
+            return new Transform();
+        }
+
+        public Transform Combine(Transform other)
+        {
+            return other;
+        }
+    }
+}
