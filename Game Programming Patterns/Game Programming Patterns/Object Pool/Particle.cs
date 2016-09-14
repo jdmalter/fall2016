c6212db4fa808928c257fb2ca191d3b532fb2932
Jacob Malter <jdmalter@ncsu.edu>
2016-09-14 11:49:19 -0400

    Added object pool.

---
+++ Game Programming Patterns/Game Programming Patterns/Object Pool/Particle.cs
@@ -0,0 +1,33 @@
+﻿namespace Game_Programming_Patterns.Object_Pool
+{
+    public class Particle
+    {
+        private int _framesLeft = 0;
+        private double _x, _y;
+        private double _xVel, _yVel;
+
+        public void Create(double x, double y, double xVel, double yVel, int lifetime)
+        {
+            _x = x;
+            _y = y;
+            _xVel = xVel;
+            _yVel = yVel;
+            _framesLeft = lifetime;
+        }
+
+        public bool Animate()
+        {
+            if (!InUse) return false;
+
+            _framesLeft--;
+            _x += _xVel;
+            _y += _yVel;
+
+            return _framesLeft == 0;
+        }
+
+        public bool InUse { get { return _framesLeft > 0; } }
+
+        public Particle Next { get; set; } = null;
+    }
+}
---
+++ Game Programming Patterns/Game Programming Patterns/Object Pool/ParticlePool.cs
@@ -0,0 +1,40 @@
+﻿namespace Game_Programming_Patterns.Object_Pool
+{
+    public class ParticlePool
+    {
+        private static readonly int POOL_SIZE = 100;
+        private Particle[] _particles = new Particle[POOL_SIZE];
+        private Particle _firstAvailable;
+
+        public ParticlePool()
+        {
+            _particles[0] = new Particle();
+            _firstAvailable = _particles[0];
+
+            for (int i = 1; i < POOL_SIZE; i++)
+            {
+                _particles[i] = new Particle();
+                _particles[i - 1].Next = _particles[i];
+            }
+        }
+
+        public void Create(double x, double y, double xVel, double yVel, int lifetime)
+        {
+            Particle particle = _firstAvailable;
+            _firstAvailable = particle.Next;
+            particle.Create(x, y, xVel, yVel, lifetime);
+        }
+
+        public void Animate()
+        {
+            foreach (Particle particle in _particles)
+            {
+                if (particle.Animate())
+                {
+                    particle.Next = _firstAvailable;
+                    _firstAvailable = particle;
+                }
+            }
+        }
+    }
+}
